d96e0ce0f771afd79efd5575aa3062e4d9449850
reneignacio <reneignaciosepulvedajara@gmail.com>
2024-03-19 17:08:54 -0300

    actualizando

--- vignette/example.R
+++ vignette/example.R
@@ -5,19 +5,16 @@ library(R.utils)
 directorioBase <- "D:/INIA/ICON/prueba_2/20240125/12"
 rutas <- list.files(directorioBase, full.names = TRUE, recursive = TRUE)
 
-#Filtrar rutas .bz2 y extraer en paralelo
+#Filtrar rutas .bz2 y extraer grib2 de los bz2
 rutas_bz2 <- rutas[grepl("\\.bz2$", rutas)]
 extraerBZ2(rutas = rutas_bz2, parallel = TRUE, ncores = 8)
-
 
 #Actualizar rutas después de la extracción
 rutas <- list.files(directorioBase, full.names = TRUE, recursive = TRUE)
 rutas_grib2 <- rutas[grepl("\\.grib2$", rutas)]
 
-#Preparar las rutas de salida .nc correspondientes
-rutass_nc <- gsub("\\.grib2$", ".nc", rutas_grib2)
-
-ruta_script_wsl = "/home/inia/ICON/triangular_a_lat_lon/0125/transform_0125.sh"
+rutass_nc <- gsub("\\.grib2$", ".nc", rutas_grib2) #Rutas de salida (.nc)
 
 
-Grib2ANetCDF(ruta_in = rutas_grib2, ruta_out = rutass_nc, parallel = T,verbose = T,ruta_script = ruta_script_wsl)
+ruta_script_wsl = "/home/inia/ICON_0125/transform_0125.sh"
+Grib2ANetCDF(ruta_in = rutas_grib2, ruta_out = rutass_nc, parallel = T,verbose = F,ruta_script = ruta_script_wsl)
